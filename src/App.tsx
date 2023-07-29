afb9eafbb54bdb4360f89561b3e24810b35d4b1b
Amber M <amberswieczkowski@pm.me>
2023-07-29 18:00:59 -0500

    Add ListGroup component with bootstrap (for sidebar).

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 /** @format */
 
-import Message from './Message';
+import ListGroup from "./components/ListGroup";
 
 function App() {
   return (
     <div>
-      <Message />
+      <ListGroup />
     </div>
   );
 }
