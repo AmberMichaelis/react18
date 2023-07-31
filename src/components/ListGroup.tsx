f1c81354b9493be01979ffb70627ea92d2749d08
Amber M <amberswieczkowski@pm.me>
2023-07-30 19:21:03 -0500

    Add conditional rendering and state functionality to ListGroup.

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,35 @@
 /** @format */
 
+import { useState } from "react";
+
 function ListGroup() {
+  const items = ['New York', 'San Francisco', 'London', 'Paris', 'Budapest'];
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  const getMessage = () => {
+    return items.length == 0 ? <p>No Items Found</p> : null;
+  };
+
   return (
-    <ul className='list-group'>
-      <li className='list-group-item'>An item</li>
-      <li className='list-group-item'>A second item</li>
-      <li className='list-group-item'>A third item</li>
-      <li className='list-group-item'>A fourth item</li>
-      <li className='list-group-item'>And a fifth one</li>
-    </ul>
+    <>
+      <h1>List</h1>
+      {getMessage()}
+      <ul className='list-group'>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? 'list-group-item active'
+                : 'list-group-item'
+            }
+            key={item}
+            onClick={() => (setSelectedIndex(index))}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
